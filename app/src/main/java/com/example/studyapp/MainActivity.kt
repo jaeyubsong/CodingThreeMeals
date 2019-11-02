6f3ef7136b683ce7bed1d62d45aa552ef071e2a4
elliekimkaist <ywkim9810@kaist.ac.kr>
2019-11-02 18:55:30 +0900

    alarm clock setting at random time

--- app/src/main/java/com/example/studyapp/MainActivity.kt
+++ app/src/main/java/com/example/studyapp/MainActivity.kt
@@ -174,9 +174,9 @@ class MainActivity : AppCompatActivity() {
 
     @Override
     fun onTimeSet() {
-        var hourOfDay: Int = 15
-        var minute: Int = 53
-        var second: Int = 0
+        var hourOfDay: Int = (12..20).random()
+        var minute: Int = (0..60).random()
+        var second: Int = (0..60).random()
 
         var c: Calendar = Calendar.getInstance()
         c.set(Calendar.HOUR_OF_DAY, hourOfDay)
